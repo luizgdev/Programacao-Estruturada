a1379af764e695f0e077dcf6fd6204551607dcc4
Luiz Gustavo da Costa Gonçalves <luizg.dev@gmail.com>
2021-08-21 12:43:31 -0300

    Atividade Funções

---
+++ Atividade Funções/Ex1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <locale.h>
+#define MAX 5
+double calculaMedia(double *alturas)
+{
+    double func_soma = 0, func_media = 0;
+    for (int i = 0; i < MAX; i++)
+        func_soma += alturas[i];
+    func_media = (func_soma / MAX);
+    return func_media;
+}
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    double alturas[MAX], media;
+    for (int i = 0; i < MAX; i++)
+    {
+        printf("Digite a %dª altura: ", (i + 1));
+        scanf("%lf", &alturas[i]);
+    }
+    media = calculaMedia(alturas);
+    printf("A média aritmética das %d alturas é: %.2lf metros.\n", MAX, media);
+    return 0;
+}
