22963b13f41d1e857b60e353ca96b9e07b18a30a
Luiz Gustavo da Costa Gonçalves <luizg.dev@gmail.com>
2021-10-01 19:44:40 -0300

    Update Ex4.c

--- Listas/Lista 01 - Vetores/Ex4.c
+++ Listas/Lista 01 - Vetores/Ex4.c
@@ -5,12 +5,11 @@ int main()
 {
     setlocale(LC_ALL, "Portuguese");
     char letras[30];
-    int i;
     printf("Insira um vetor de até 30 letras: ");
     fflush(stdin);
     scanf("%[^\n]", letras);
     printf("A ordem inversa do vetor é: ");
-    for (i = strlen(letras); i >= 0; i--)
+    for (int i = strlen(letras); i >= 0; i--)
         printf("%c", letras[i]);
     printf("\n");
     return 0;
