f13032e5de2a29d6b582e0fc86e81b3965df0628
Luiz Gustavo da Costa Gonçalves <luizg.dev@gmail.com>
2021-10-01 19:41:51 -0300

    Update Ex2.c

--- Listas/Lista 01 - Vetores/Ex2.c
+++ Listas/Lista 01 - Vetores/Ex2.c
@@ -3,16 +3,16 @@
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int i, quantidade_de_alunos;
-    printf("Digite a quantidade de alunos da turma: ");
+    int quantidade_de_alunos;
+    printf("Informe a quantidade de alunos da turma: ");
     scanf("%d", &quantidade_de_alunos);
     double notas[quantidade_de_alunos];
-    for (i = 0; i < quantidade_de_alunos; i++)
+    for (int i = 0; i < quantidade_de_alunos; i++)
     {
         printf("Digite a nota do %dº aluno: ", (i + 1));
         scanf("%lf", &notas[i]);
     }
-    for (i = 0; i < quantidade_de_alunos; i++)
-        printf("%dª nota = %.2lf\n", (i + 1), notas[i]);
+    for (int i = 0; i < quantidade_de_alunos; i++)
+        printf("%dª nota: %.2lf\n", (i + 1), notas[i]);
     return 0;
 }
