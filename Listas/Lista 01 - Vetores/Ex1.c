bc87d023ce092587ffba0e8360d1c35253cb59be
Luiz Gustavo da Costa Gonçalves <luizg.dev@gmail.com>
2021-10-01 19:39:33 -0300

    Update Ex1.c

--- Listas/Lista 01 - Vetores/Ex1.c
+++ Listas/Lista 01 - Vetores/Ex1.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <locale.h>
+
+#define MAX 30
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int i;
-    double notas[30];
-    for (i = 0; i < 30; i++)
+    double notas[MAX];
+    for (int i = 0; i < MAX; i++)
     {
         printf("Digite a nota do %dº aluno: ", (i + 1));
         scanf("%lf", &notas[i]);
     }
-    for (i = 0; i < 30; i++)
-        printf("%dª nota = %.2lf\n", (i + 1), notas[i]);
+    for (int i = 0; i < MAX; i++)
+        printf("%dª nota: %.2lf\n", (i + 1), notas[i]);
     return 0;
 }
