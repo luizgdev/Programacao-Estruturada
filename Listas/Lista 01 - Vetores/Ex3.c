64b7dba47f3f055afb99ace48227c9cd7351dc4d
Luiz Gustavo da Costa Gonçalves <luizg.dev@gmail.com>
2021-10-01 19:43:01 -0300

    Update Ex3.c

--- Listas/Lista 01 - Vetores/Ex3.c
+++ Listas/Lista 01 - Vetores/Ex3.c
@@ -3,8 +3,8 @@
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int soma = 0, dados[100], i;
-    for (i = 0; i < 100; i++)
+    int soma = 0, dados[100];
+    for (int i = 0; i < 100; i++)
     {
         printf("Insira o %dº valor a ser somado: ", (i + 1));
         scanf("%d", &dados[i]);
