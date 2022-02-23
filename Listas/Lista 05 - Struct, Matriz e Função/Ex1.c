16e2aa996adb857fb69a8e90bd6925874f61419d
Luiz Gustavo da Costa Gonçalves <luizg.dev@gmail.com>
2022-02-23 11:14:49 -0300

    ex1

--- Listas/Lista 05 - Struct, Matriz e Função/Ex1.c
+++ Listas/Lista 05 - Struct, Matriz e Função/Ex1.c
@@ -2,18 +2,42 @@
 #include <stdlib.h>
 #include <locale.h>
 
+#define L 2
+#define C 2
+
 typedef struct
 {
     int codigo;
     char nome[50 + 1];
     double preco;
     int baixa;
-} PRODUTO;
+} PRODUTO[L][C];
+
+void leitura(PRODUTO *ptr)
+{
+    for (int i = 0; i < L; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            puts("codigo:");
+            scanf("%d", &ptr[i][j]->codigo);
+            puts("nome:");
+            getchar();
+            fgets(ptr[i][j]->nome, 50 + 1, stdin);
+            puts("preço:");
+            scanf("%lf", &ptr[i][j]->preco);
+            puts("baixa:");
+            scanf("%d", &ptr[i][j]->baixa);
+        }
+    }
+}
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    PRODUTO dados[4][6];
+    PRODUTO dados;
+
+    leitura(&dados);
 
     return 0;
 }
